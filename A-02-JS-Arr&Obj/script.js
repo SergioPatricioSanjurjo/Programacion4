6abaebab6c9fe6b8034530507df7d5f5b232628f
Sergio Sanjurjo <86019664+SergioPatricioSanjurjo@users.noreply.github.com>
2023-08-31 11:30:55 -0300

    Add files via upload

---
+++ A-02-JS-Arr&Obj/script.js
@@ -0,0 +1,20 @@
+//! --------------------Trabajo practico nº 2 – Arreglos - Objetos--------------------
+//! ----------Arreglos con Tipos de Datos Primitivos:----------
+//? 1. Suma de Elementos: Dado un arreglo de números, calcula la suma de todos los elementos.
+
+//? 2. Promedio: Calcula el promedio de los números en un arreglo.
+//? 3. Máximo y Mínimo: Encuentra el número máximo y el número mínimo en un arreglo de números.
+//? 4. Buscar Valor: Escribe una función que busque un valor específico en un arreglo y devuelva su índice, si existe.
+//? 5. Filtrar Pares e Impares: Separa un arreglo de números en dos arreglos diferentes, uno con números pares y otro con números impares.
+//? 6. Eliminar Duplicados: Crea una función que elimine los elementos duplicados de un arreglo.
+//? 7. Ordenar Arreglo: Ordena un arreglo de números de forma ascendente.
+//? 8. Eliminar Valor: Elimina todas las ocurrencias de un valor específico de un arreglo.
+//? 9. Combinar Arreglos: Combina dos arreglos en uno solo, asegurándote de que no haya duplicados.
+//! ----------Arreglos con Tipos de Datos Compuestos:----------
+//? 10. Lista de Compras: Crea una lista de compras que incluya productos y sus cantidades.
+//? 11. Agenda de Contactos: Crea una agenda de contactos con nombres, números de teléfono y correos electrónicos.
+//? 12. Búsqueda de Palabras: Dado un párrafo y una palabra, cuenta cuántas veces aparece la palabra en el párrafo.
+//? 13. Clasificación de Objetos: Ordena un arreglo de objetos por una propiedad específica.
+//? 14. Registro de Ventas: Crea un registro de ventas con información sobre productos, cantidades y precios.
+//? 15. Carrito de Compras: Simula un carrito de compras que permite agregar, eliminar y calcular el total.
+//? 16. Gestor de Tareas: Crea un gestor de tareas que permita agregar, completar y listar tareas pendientes.
